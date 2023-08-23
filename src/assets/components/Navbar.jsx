a27f2e506b8913ef52982b83456f796b9bff600e
FedeOrefici <federicoaorefici@gmail.com>
2023-08-23 14:46:31 -0300

    update

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -1,18 +1,24 @@
-import { Box, Text, Input, Button } from '@chakra-ui/react'
+import { Stat, StatLabel, StatNumber, StatArrow, StatHelpText, Box } from '@chakra-ui/react'
 
 const Navbar = () => {
+
+
 
 
   return (
     <Box h='100vh' border='1px solid green' w='15%' p='20px'>
       <Box>
-        <Box>
-          <span class="material-symbols-outlined">real_estate_agent</span>
-        </Box>
-        <Box w='80%' display='flex' flexDirection='column' gap='10px'>
-          <Input />
-          <Button>search</Button>
-        </Box>
+        <Stat>
+          <StatLabel>dolar price</StatLabel>
+          <StatNumber>300,80</StatNumber>
+            <StatHelpText>
+              <StatArrow type='increase'></StatArrow>
+              0,3%
+            </StatHelpText>
+        </Stat>
+      </Box>
+      <Box>
+       filters
       </Box>
     </Box>
   )
