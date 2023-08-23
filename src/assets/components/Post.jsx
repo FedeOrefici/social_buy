6cd9214e41cd15624fe89fca79af1d9ce394cbc9
FedeOrefici <federicoaorefici@gmail.com>
2023-08-23 13:56:18 -0300

    update

--- src/assets/components/Post.jsx
+++ src/assets/components/Post.jsx
@@ -1,46 +1,71 @@
 import React from 'react'
 import { Card, CardBody, Text, Image, Box, Badge, Avatar } from '@chakra-ui/react'
 
+
 const Post = () => {
 
     const profiles = [{
       imageUrl: 'https://bit.ly/2Z4KKcF',
-      name: 'House with two floors',
+      name: 'House',
       status: 'on sale',
+      seller: 'Mark',
       description: 'Donec vulputate ac justo eu blandit. Nullam porttitor, enim in volutpat finibus, nulla metus malesuada purus, eu condimentum arcu purus eu tellus. Morbi in mi in sapien dictum laoreet. Sed viverra id ex eget blandit. Ut eget fermentum sapien. Donec sed lorem quis lorem pretium sodales. Aenean eu purus vel enim cursus sagittis non eu nunc. Suspendisse nibh lorem, tincidunt sit amet tristique a, commodo ac lorem. Duis consectetur nulla eu maximus pretium. Nullam non ullamcorper ipsum.'
     },
     {
       imageUrl: 'https://bit.ly/2Z4KKcF',
-      name: 'House on the beach',
+      name: 'Building',
       status: 'negociations',
+      seller: 'Maria',
       description: 'Donec vulputate ac justo eu blandit. Nullam porttitor, enim in volutpat finibus, nulla metus malesuada purus, eu condimentum arcu purus eu tellus. Morbi in mi in sapien dictum laoreet. Sed viverra id ex eget blandit. Ut eget fermentum sapien. Donec sed lorem quis lorem pretium sodales. Aenean eu purus vel enim cursus sagittis non eu nunc. Suspendisse nibh lorem, tincidunt sit amet tristique a, commodo ac lorem. Duis consectetur nulla eu maximus pretium. Nullam non ullamcorper ipsum.'
     },
     {
       imageUrl: 'https://bit.ly/2Z4KKcF',
-      name: 'House in the city',
+      name: 'House',
       status: 'sold',
+      seller: 'Christian',
+      description: 'Donec vulputate ac justo eu blandit. Nullam porttitor, enim in volutpat finibus, nulla metus malesuada purus, eu condimentum arcu purus eu tellus. Morbi in mi in sapien dictum laoreet. Sed viverra id ex eget blandit. Ut eget fermentum sapien. Donec sed lorem quis lorem pretium sodales. Aenean eu purus vel enim cursus sagittis non eu nunc. Suspendisse nibh lorem, tincidunt sit amet tristique a, commodo ac lorem. Duis consectetur nulla eu maximus pretium. Nullam non ullamcorper ipsum.'
+  },
+  {
+      imageUrl: 'https://bit.ly/2Z4KKcF',
+      name: 'House',
+      status: 'on sale',
+      seller: 'Alex',
+      description: 'Donec vulputate ac justo eu blandit. Nullam porttitor, enim in volutpat finibus, nulla metus malesuada purus, eu condimentum arcu purus eu tellus. Morbi in mi in sapien dictum laoreet. Sed viverra id ex eget blandit. Ut eget fermentum sapien. Donec sed lorem quis lorem pretium sodales. Aenean eu purus vel enim cursus sagittis non eu nunc. Suspendisse nibh lorem, tincidunt sit amet tristique a, commodo ac lorem. Duis consectetur nulla eu maximus pretium. Nullam non ullamcorper ipsum.'
+  },
+  {
+      imageUrl: 'https://bit.ly/2Z4KKcF',
+      name: 'House',
+      status: 'on negotiations',
+      seller: 'Frank',
       description: 'Donec vulputate ac justo eu blandit. Nullam porttitor, enim in volutpat finibus, nulla metus malesuada purus, eu condimentum arcu purus eu tellus. Morbi in mi in sapien dictum laoreet. Sed viverra id ex eget blandit. Ut eget fermentum sapien. Donec sed lorem quis lorem pretium sodales. Aenean eu purus vel enim cursus sagittis non eu nunc. Suspendisse nibh lorem, tincidunt sit amet tristique a, commodo ac lorem. Duis consectetur nulla eu maximus pretium. Nullam non ullamcorper ipsum.'
   }]
 
 
 
   return (
-    <Box w='full' border='1px solid teal' display='flex' alignItems='center' justifyContent='center' flexDirection='column'>
+    <Box w='full' display='flex' alignItems='center' justifyContent='center' flexDirection='column' overflowY='scroll' h='100vh' gap='20px' py='100px'>
     {profiles.map((user) => (
-        <Card w='70%' h='400px' display='flex' justifyContent='center' alignItems='center' border='1px solid black' p='40px'>
-        <CardBody display='flex' alignItems='center' justifyContent='space-around' w='100%' p='10px' border='1px solid green'>
-          <Box pos='relative' bottom='100px' w='50%'>
-            <Avatar size='sm' src='https://bit.ly/dan-abramov'/>
-            <Box><Badge borderRadius='full' px='2' colorScheme='teal'>{user.status}</Badge></Box>
-            <Text>{user.name}</Text>
+        <Card w='70%' h='400px' display='flex' justifyContent='center' alignItems='center' p='40px'>
+        <CardBody data-aos="flip-left" display='flex' alignItems='center' justifyContent='space-around' w='100%' p='10px'>
+          <Box w='20%' h='90%' display='flex' flexDirection='column' p='30px' alignItems='center' justifyContent='center'>
+            <Avatar size='md' src='https://bit.ly/dan-abramov'/>
+            <Text fontWeight='bold'>{user.seller}</Text>
           </Box>
-          <Box border='1px solid red' w='50%'>
+          <Box h='90%' w='80%' p='10px' display='flex' alignItems='center' justifyContent='center' gap='30px'>
             <Image w='300px' src={user.imageUrl} />
-            <Text>{user.description}</Text>
+            <Box w='60%'>
+              <Box display='flex' gap='10px'>
+                <Text>{user.name}</Text>
+                <Badge borderRadius='full' px='2' display='flex' alignItems='center' fontSize='10px' colorScheme='teal'>{user.status}</Badge>
+              </Box>
+              <Text textAlign='justify'>{user.description}</Text>
+            </Box>
           </Box>
         </CardBody>
       </Card>
+
     ))}
+     
     </Box>
   )
 }
