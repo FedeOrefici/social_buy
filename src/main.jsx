a892c5684f26a0c9675647b601b110d7f97cb16b
FedeOrefici <federicoaorefici@gmail.com>
2023-08-24 15:44:17 -0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,16 @@ import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import { BrowserRouter } from 'react-router-dom'
 import DataProvider from './context/ContextData.jsx'
+import UserProvider from './context/UsersData.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <ChakraProvider>
       <DataProvider>
+      <UserProvider>
         <App />
+      </UserProvider>
       </DataProvider>
       </ChakraProvider>
     </BrowserRouter>
