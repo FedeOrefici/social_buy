f9b3ab109b8f279c80d00cd30d5429be787bb6d5
FedeOrefici <federicoaorefici@gmail.com>
2023-08-24 16:24:18 -0300

    update

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -5,7 +5,6 @@ import data from '../data/data.json'
 const Post = () => {
 
   
-  console.log(data)
 
 
   return (
@@ -15,16 +14,19 @@ const Post = () => {
         <CardBody data-aos="flip-left" display='flex' alignItems='center' justifyContent='space-around' w='100%' p='10px'>
           <Box w='20%' h='90%' display='flex' flexDirection='column' p='30px' alignItems='center' justifyContent='center'>
             <Avatar size='md' src='https://bit.ly/dan-abramov'/>
-            <Text fontWeight='bold'>{user.seller}</Text>
+            <Text fontWeight='bold' textAlign='center' w='200px' py='4px'>{user.seller}</Text>
           </Box>
           <Box h='90%' w='80%' p='10px' display='flex' alignItems='center' justifyContent='center' gap='30px'>
-            <Image w='300px' src={user.imageUrl} />
+            <Image w='300px' src={user.imageUrl} h='200' borderRadius='3px' />
             <Box w='60%'>
               <Box display='flex' gap='10px'>
                 <Text>{user.name}</Text>
-                <Badge borderRadius='full' px='2' display='flex' alignItems='center' fontSize='10px' colorScheme='teal'>{user.status}</Badge>
+                <Badge borderRadius='full' px='2' display='flex' alignItems='center' fontSize='10px' colorScheme={user.status === 'on sale' || user.status === 'on negotiations' ? 'teal' : 'red'}>
+                  {user.status}
+                </Badge>
               </Box>
               <Text textAlign='justify'>{user.description}</Text>
+              <Text>$ {user.price}</Text>
             </Box>
           </Box>
         </CardBody>
